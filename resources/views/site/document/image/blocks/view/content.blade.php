07e39f1f5a271d894aa24c8bcce718815f75718a
vmartuniyk <martuniyk93@ukr.net>
2022-12-24 22:40:47 +0200

    Start project test git

--- resources/views/site/document/image/blocks/view/content.blade.php
+++ resources/views/site/document/image/blocks/view/content.blade.php
@@ -39,7 +39,7 @@
 
         @include('site.document.text.blocks.view.comment')
         <div class="show-more-btn">
-            <a href="#">View Similar Entries Git</a>
+            <a href="#">View Similar Entries </a>
         </div>
     </div>
 </div>
