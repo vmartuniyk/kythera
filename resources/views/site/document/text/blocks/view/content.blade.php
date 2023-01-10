b4243560e4c7eedfca9bca40bb192ea97711fe5c
vmartuniyk <martuniyk93@ukr.net>
2023-01-10 22:52:09 +0200

    New layouts for text

--- resources/views/site/document/text/blocks/view/content.blade.php
+++ resources/views/site/document/text/blocks/view/content.blade.php
@@ -1,35 +1,45 @@
-<div class="txtdoc-view text">
-    <div class="line"></div>
 
-    <div class="txtdoc clearfix">
-        {{-- xmenu::entry_edit($item->user_id, $item) --}}
-        <div>
-        	<h2>
-            @if (Config::get('app.debug'))
-            {!!$item->id!!}:
-            @endif
-            {!! xhtml::crumbs(Router::getSelected(), ' &gt; ', false) !!}
-            </h2>
-            {{-- <p class="author">{!! trans('locale.submitted', array('fullname'=>xhtml::fullname($item, false), 'date'=>$item->created_at->format('d.m.Y'))) !!}</p> --}}
-            {!! xmenu::author($item) !!}
-            <h1>{{ $item->title }}</h1>
+<div class="inner-page__content content-inner entry-page">
+    <div class="content-inner__wrap">
+        <div class="entry-card">
+            <div class="entry-card__image">
+                @if($item->image)
+                    <picture><source srcset="{{ $item->cache }}" type="image/webp">
+                        <img src="{{ $item->cache }}" alt=""></picture>
+                @else
+                    <picture><source srcset="{{ URL::asset('assets/img/history.webp') }}" type="image/webp">
+                        <img src="{{ URL::asset('assets/img/history.jpg?_v=1657459303074') }}" alt=""></picture>
+
+                @endif
+
+            </div>
+            <div class="entry-card__footer">
+                <div class="entry-card__publication">
+                    <div class="entry-card__date">
+                        <time datetime="{!! $item->created_at->format('d.m.Y') !!}">{!! $item->created_at->format('d.m.Y') !!}</time> &bull;
+                        <span class="entry-card__autor"> {!! $item->firstname . " ". $item->lastname !!}</span>
+                    </div>
+
+                </div>
+                <div class="entry-card__activity">
+                    <div class="entry-card__views">
+                        <span>2,445</span> <span>Views</span>
+                    </div>
+                    <div class="entry-card__comments"><span>3</span> Comments</div>
+                </div>
+            </div>
+        </div>
+        <section class="content-inner__text inner-main-text">
+            <h1 class="inner-main-text__title">{{ $item->title }}</h1>
+            <p class="inner-main-text__paragraf">
+                {!!  $item->content !!}
+            </p>
+
+        </section>
+
+        @include('site.document.text.blocks.view.comment')
+        <div class="show-more-btn">
+            <a href="#">View Similar Entries </a>
         </div>
     </div>
-<?php //echo '<pre>'; print_r($item); die;?>
-    @if($item->image)
-        <div class="documentTextImage">
-            @if ( !empty($item->lightbox) )
-                <a href="{!!$item->lightbox!!}" data-lightbox="{!!$item->title!!}" data-title="{!!$item->title!!}">
-                    <img alt="{!!$item->title!!}" src="{!!$item->image!!}" />
-                </a>
-            
-            @endif
-            @if ( !empty($item->copyright) )
-                <br/><span class="copyright">{!!$item->copyright!!}</span>
-            @endif
-            <div class="line"></div>
-        </div>
-    @endif
-
-    <p>{!!$item->content!!}</p>
 </div>
--- resources/views/site/document/text/view.blade.php
+++ resources/views/site/document/text/view.blade.php
@@ -1,4 +1,4 @@
-@extends('site.layout.default')
+@extends('site.layout.default-new')
 
 @section('title')
 	{{ $page->title }} - {{ $item->title }}
@@ -9,38 +9,50 @@
 
 
 @section('content')
-    <div class="container">
+{{--    <div class="container">--}}
 
-        <div class="head">
-            @include('site.document.text.blocks.index.head')
-        </div>
+{{--        <div class="head">--}}
+{{--            @include('site.document.text.blocks.index.head')--}}
+{{--        </div>--}}
 
-        <div class="content">
-            <!-- content -->
+{{--        <div class="content">--}}
+{{--            <!-- content -->--}}
 
-            <div class="col-md-8 col2">
-                {{-- xmenu::entry_edit($item->user_id, $item) --}}
-                @if(Session::has('global'))<p class="bg-info">{!! Session::get('global') !!}</p>@endif
-                
-                <!-- txtdoc -->
+{{--            <div class="col-md-8 col2">--}}
+{{--                --}}{{-- xmenu::entry_edit($item->user_id, $item) --}}
+{{--                @if(Session::has('global'))<p class="bg-info">{!! Session::get('global') !!}</p>@endif--}}
+{{--                --}}
+{{--                <!-- txtdoc -->--}}
+{{--                @include('site.document.text.blocks.view.content')--}}
+{{--                <!-- /txtdoc -->--}}
+{{--                <!-- comment -->--}}
+{{--                @include('site.document.text.blocks.view.comment')--}}
+{{--                <!-- /comment -->--}}
+{{--            </div>--}}
+
+
+{{--            <div class="col-md-4 sidebar">--}}
+{{--                <!-- sidebar -->--}}
+{{--                @if (1||!Config::get('app.debug'))--}}
+{{--					@include('site.document.text.blocks.sidebar')--}}
+{{--				@endif--}}
+{{--                <!-- /sidebar -->--}}
+{{--            </div>--}}
+
+{{--            <!-- /content -->--}}
+{{--        </div>--}}
+
+{{--    </div>--}}
+<main class="page">
+    <div class="inner-page">
+        <div class="inner-page__container">
+            <div class="inner-page__wrap">
+                @include('partials.front.left-front-menu')
+
                 @include('site.document.text.blocks.view.content')
-                <!-- /txtdoc -->
-                <!-- comment -->
-                @include('site.document.text.blocks.view.comment')
-                <!-- /comment -->
+
             </div>
-
-
-            <div class="col-md-4 sidebar">
-                <!-- sidebar -->
-                @if (1||!Config::get('app.debug'))
-					@include('site.document.text.blocks.sidebar')
-				@endif
-                <!-- /sidebar -->
-            </div>
-
-            <!-- /content -->
         </div>
-
     </div>
+</main>
 @stop
