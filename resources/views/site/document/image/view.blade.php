67d890d00455fb6bfc14d36dfe283d18fce6b5cb
vmartuniyk <martuniyk93@ukr.net>
2023-01-14 18:53:29 +0200

    Fix left menu link when post has image refactor

--- resources/views/site/document/image/view.blade.php
+++ resources/views/site/document/image/view.blade.php
@@ -23,50 +23,15 @@
     @endif
 @endsection
 
-@section('style')
-@stop
-
 
 @section('content')
-    {{-- <div class="container">
-        <div class="head">
-            @include('site.document.text.blocks.index.head')
-        </div>
-
-        <div class="content">
-            <!-- content -->
-
-            <div class="col-md-8 col2">
-                @if(Session::has('global'))<p class="bg-info">{!! Session::get('global') !!}</p>@endif
-                
-                <!-- txtdoc -->
-                @include('site.document.image.blocks.view.content')
-                <!-- /txtdoc -->
-                <!-- comment -->
-                @include('site.document.text.blocks.view.comment')
-                <!-- /comment -->
-            </div>
-
-
-            <div class="col-md-4 sidebar">
-                <!-- sidebar -->
-                @include('site.document.text.blocks.sidebar')
-                <!-- /sidebar -->
-            </div>
-
-            <!-- /content -->
-        </div>
-
-    </div> --}}
-
-    <main class="page">
+      <main class="page">
         <div class="inner-page">
             <div class="inner-page__container">
                 <div class="inner-page__wrap">
                     @include('partials.front.left-front-menu')
 
                     @include('site.document.image.blocks.view.content')
-
                 </div>
             </div>
         </div>
