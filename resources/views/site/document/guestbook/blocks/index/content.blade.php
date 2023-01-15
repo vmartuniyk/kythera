67d0ff6f7a7edbcec1d375b7a2cc6a18343e4058
vmartuniyk <martuniyk93@ukr.net>
2023-01-15 20:38:02 +0200

    Guest book new list

--- resources/views/site/document/guestbook/blocks/index/content.blade.php
+++ resources/views/site/document/guestbook/blocks/index/content.blade.php
@@ -1,11 +1,11 @@
 <div class="txtdoc-list clearfix">
-    <div class="line"></div>
+{{--    <div class="line"></div>--}}
 
-    @if(!empty($page->content))
-    <p>{!!$page->content!!}</p>
-    <br/>
-    <div class="line"></div>
-    @endif
+{{--    @if(!empty($page->content))--}}
+{{--    <p>{!!$page->content!!}</p>--}}
+{{--    <br/>--}}
+{{--    <div class="line"></div>--}}
+{{--    @endif--}}
 
 
     @if(Session::has('global'))<p class="bg-info">{!! Session::get('global') !!}</p>@endif
@@ -29,28 +29,71 @@
 
         <div class="line"></div>
 
-        @foreach($items as $item)
-        <div class="txtdoc text clearfix">
-            {!! xmenu::entry_edit($item->item->persons_id, $item) !!}
-            <div>
-            	<h2>
-                @if (Config::get('app.debug'))
-                {!!$item->id!!}
-                @endif
-                {{ $item->title }}
-                </h2>
-                <span class="date">{!!$item->date!!}</span>
-                <p>{!!$item->content!!}</p>
-                @if ($email = $item->entry->getEmail())
-                <a href="{!!action('DocumentGuestbookController@contact',$item->id)!!}">contact</a>
-                @endif
-                </div>
-        </div>
-        @endforeach
+{{--        @foreach($items as $item)--}}
+{{--        <div class="txtdoc text clearfix">--}}
+{{--            {!! xmenu::entry_edit($item->item->persons_id, $item) !!}--}}
+{{--            <div>--}}
+{{--            	<h2>--}}
+{{--                @if (Config::get('app.debug'))--}}
+{{--                {!!$item->id!!}--}}
+{{--                @endif--}}
+{{--                {{ $item->title }}--}}
+{{--                </h2>--}}
+{{--                <span class="date">{!!$item->date!!}</span>--}}
+{{--                <p>{!!$item->content!!}</p>--}}
+{{--                @if ($email = $item->entry->getEmail())--}}
+{{--                <a href="{!!action('DocumentGuestbookController@contact',$item->id)!!}">contact</a>--}}
+{{--                @endif--}}
+{{--                </div>--}}
+{{--        </div>--}}
+{{--        @endforeach--}}
 
-        <span class="personal pull-right">{!!$pages->render()!!}</span>
+{{--        <span class="personal pull-right">{!!$pages->render()!!}</span>--}}
     @else
         <p>There are currently no entries in this section.</p>
     @endif
+
+    <section class="content-inner__articles inner-articles">
+        <div class="inner-articles__cards">
+            @foreach($items as $i=>$item)
+
+                <article class="inner-articles__card card-articles">
+
+                    <div class="card-articles__info" style="margin-left: 0px">
+                        {!! xmenu::entry_edit($item->item->persons_id, $item) !!}
+                        <h4 class="card-articles__title">
+                              {!!$item->title!!}
+                        </h4>
+                        <p class="card-articles__text">
+                            {!! $item->content !!}
+                        </p>
+                        @if ($email = $item->entry->getEmail())
+                            <a href="{!!action('DocumentGuestbookController@contact',$item->id)!!}" class="card-articles__link view-link">
+                                contact
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.132 16.693">
+                                    <g data-name="Group 23" transform="translate(-1292.865 -1328.645)">
+                                        <g data-name="Group 22">
+                                            <path data-name="Path 40" d="M1307.614,1328.991l7.691,8-7.691,8" fill="none" stroke="#24646d" stroke-miterlimit="10" stroke-width="1" />
+                                        </g>
+                                        <line data-name="Line 3" x1="21.613" transform="translate(1292.865 1336.991)" fill="none" stroke="#24646d" stroke-miterlimit="10" stroke-width="1" />
+                                    </g>
+                                </svg>
+                            </a>
+                        @endif
+
+                        <div class="card-articles__footer">
+                            <div class="card-articles__date">
+                                <time datetime="{!! $item->date !!}">{!! $item->date !!}</time>
+                            </div>
+
+                        </div>
+                    </div>
+                </article>
+
+            @endforeach
+        </div>
+        <span class="pull-pagination">{!! $pages->render() !!}</span>
+
+    </section>
 </div>
 
--- resources/views/site/document/guestbook/index.blade.php
+++ resources/views/site/document/guestbook/index.blade.php
@@ -1,4 +1,4 @@
-@extends('site.layout.default')
+@extends('site.layout.default-new')
 
 @section('title')
     {{ $page->title }}
@@ -65,39 +65,62 @@ span.date {
 @stop
 
 @section('content')
-    <div class="container">
+{{--    <div class="container">--}}
 
-        <div class="head">
-            @include('site.document.text.blocks.index.head')
-        </div>
+{{--        <div class="head">--}}
+{{--            @include('site.document.text.blocks.index.head')--}}
+{{--        </div>--}}
 
-        <div class="content">
-            <!-- content -->
+{{--        <div class="content">--}}
+{{--            <!-- content -->--}}
 
-            <div class="col-md-8 col2 guestbook">
-                <!-- txtdoc -->
-                @include('site.document.guestbook.blocks.index.content')
-                <!-- /txtdoc -->
+{{--            <div class="col-md-8 col2 guestbook">--}}
+{{--                <!-- txtdoc -->--}}
+{{--                @include('site.document.guestbook.blocks.index.content')--}}
+{{--                <!-- /txtdoc -->--}}
 
-                <hr class="blue"/>
+{{--                <hr class="blue"/>--}}
 
-                <!-- create -->
-                @include('site.document.guestbook.blocks.index.create')
-                <!-- /create -->
+{{--                <!-- create -->--}}
+{{--                @include('site.document.guestbook.blocks.index.create')--}}
+{{--                <!-- /create -->--}}
+{{--            </div>--}}
+
+
+{{--            <div class="col-md-4 sidebar">--}}
+{{--                @if (!Config::get('app.debug'))--}}
+{{--                    <!-- sidebar -->--}}
+{{--                    @include('site.document.text.blocks.sidebar')--}}
+{{--                    <!-- /sidebar -->--}}
+{{--                @endif--}}
+{{--            </div>--}}
+
+{{--            <!-- /content -->--}}
+{{--        </div>--}}
+
+{{--    </div>--}}
+
+    <main class="page">
+        <div class="inner-page">
+            <div class="inner-page__container">
+                <div class="inner-page__wrap">
+                    @include('partials.front.left-front-menu')
+
+                    <div class="inner-page__content content-inner text-first-screen">
+                        <div class="content-inner__wrap">
+                            @include('site.document.text.blocks.index.head')
+
+                            <section class="content-inner__articles inner-articles">
+                                @include('site.document.guestbook.blocks.index.content')
+                            </section>
+
+                            @include('site.document.guestbook.blocks.index.create')
+
+                        </div>
+                    </div>
+                </div>
             </div>
-
-
-            <div class="col-md-4 sidebar">
-                @if (!Config::get('app.debug'))
-                    <!-- sidebar -->
-                    @include('site.document.text.blocks.sidebar')
-                    <!-- /sidebar -->
-                @endif
-            </div>
-
-            <!-- /content -->
         </div>
-
-    </div>
+    </main>
 @stop
 
