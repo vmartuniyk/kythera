42d212e447c81115ace3c15d4a1ca34e4d8bad4f
vmartuniyk <martuniyk93@ukr.net>
2023-01-16 23:22:41 +0200

    Fix audio on page

--- resources/views/site/document/audio/blocks/view/content.blade.php
+++ resources/views/site/document/audio/blocks/view/content.blade.php
@@ -54,6 +54,11 @@ $count = App\Models\Comment::whereEntityCount($item)
             <a class="view audio" href="{!! $item->audio !!}" title="{{ $item->title }}">
                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAABD0lEQVRYw+3XsQqDMBAG4L6suogIIoq7ToIuDi4i6C6+gSDo6OjjXEkhoUNibcCYtPdDFk3MB8Z4eTwwGAzG7FiWBVEUwbIsoC2QtqIo7kGGYSiceBxH8DyPIZMkAeU4MvGnfmVZqkdS3BkgSdd1rH+e56AMdxZIUlUVGzNNEyjBfQN8H+/7PijB8YDbtgknX9eVjZvnGS7H8YDkWl3XwsnJ3kj6pGkqD4zjGGhzXVeIEwFJa5qGCxiG4XXftm154BHoLFC0Pvd9l1q/lwDbtoWj598OzLKMi3AcB4G/8Yov+0i032a036iN+9UZUSwYUW7JFqx936srWI0o+WmCIND30GTEsdPogzsGg/n3PAGH4GdMo71vewAAAABJRU5ErkJggg==">
             </a>
+            <div>
+            <audio controls autoplay>
+                <source src="{!! $item->audio !!}" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
             <p class="inner-main-text__paragraf">
                 {!!  $item->content !!}
             </p>
